b60e378695e561ad40d30d25d41872173b52c846
Diego Vega <diego.vega@microsoft.com>
2017-09-24 18:16:56 -0700

    Adds simple data seeding demo

---
+++ data-seeding/Migrations/20170925010903_Initial.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace dataseeding.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Themes",
+                columns: table => new
+                {
+                    ThemeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    TitleColor = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Themes", x => x.ThemeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BlogUrl = table.Column<string>(nullable: true),
+                    ThemeId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                    table.ForeignKey(
+                        name: "FK_Blogs_Themes_ThemeId",
+                        column: x => x.ThemeId,
+                        principalTable: "Themes",
+                        principalColumn: "ThemeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_ThemeId",
+                table: "Blogs",
+                column: "ThemeId");
+
+            migrationBuilder.InsertData(
+                table: "Themes",
+                columns: new[] { "ThemeId", "Name", "TitleColor" },
+                values: new object[] { 1, "MSDN", -5658199 });
+
+            migrationBuilder.InsertData(
+                table: "Themes",
+                columns: new[] { "ThemeId", "Name", "TitleColor" },
+                values: new object[] { 2, "TechNet", -16741493 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Themes");
+        }
+    }
+}
---
+++ data-seeding/Migrations/20170925011015_PersonalColor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace dataseeding.Migrations
+{
+    public partial class PersonalColor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Themes",
+                columns: new[] { "ThemeId", "Name", "TitleColor" },
+                values: new object[] { 3, "Personal", -5383962 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Themes",
+                keyColumn: "ThemeId",
+                keyValue: 3);
+        }
+    }
+}
